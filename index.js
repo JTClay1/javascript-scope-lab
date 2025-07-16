430dc2a499ad4ae139a34ae1177e3be8062c58bf
Joshua Clay <josh.t.clay1995@gmail.com>
2025-07-16 03:42:12 -0700

    Finalized JS Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Global Vars
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add a burger (Function-scoped variable)
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Block-scoped burger addition
+if(true){
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// Function to change the featured drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
